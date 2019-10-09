505e749cb2eb522886769fd1a315b4ca9c126b40
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-09 23:33:23 +0900

    heroku

--- src/Application.kt
+++ src/Application.kt
@@ -1,5 +1,6 @@
 package jp.making.felix
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
@@ -10,12 +11,19 @@ import kotlinx.html.*
 import kotlinx.css.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
+    install(ContentNegotiation){
+        jackson { {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+        } }
+    }
     val client = HttpClient(Apache) {
     }
 
@@ -33,7 +41,6 @@ fun Application.module(testing: Boolean = false) {
             )
             call.respond(response)
         }
-
     }
 }
 
