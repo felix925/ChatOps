b42a5784e56e9d81c0e12ab3e2542962a2c76c1e
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-09 23:40:51 +0900

    heroku

--- src/Application.kt
+++ src/Application.kt
@@ -16,8 +16,8 @@ import io.ktor.jackson.jackson
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
-//@kotlin.jvm.JvmOverloads
+//@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
 fun Application.module() {
     install(ContentNegotiation){
         jackson { {
