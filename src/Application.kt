c7dc5b257daa03177d7bc1c156bf0e5b8c5b88e2
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-10 11:22:17 +0900

    heroku

--- src/Application.kt
+++ src/Application.kt
@@ -3,8 +3,6 @@ package jp.making.felix
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.jackson.jackson
@@ -18,11 +16,9 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing:Boolean = false) {
+fun Application.module() {
     install(ContentNegotiation){
         jackson {}
-    }
-    val client = HttpClient(Apache) {
     }
 
     routing {
