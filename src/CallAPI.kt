93a8a00073d0f79587dae69e7389323d11ef6613
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-11 20:58:05 +0900

    GitHubAcdtionCI/CD Connect

--- src/CallAPI.kt
+++ src/CallAPI.kt
@@ -5,7 +5,7 @@ import khttp.delete as httpDelete
 class CallAPI(val organizer:String = "SoyBeansLab", val repository:String = "daizu-ChatOps") {
 
     fun CallTest(){
-        val url = "https://api.github.com/repos/${organizer}/${repository}/dispatches?token=5eb6b6a7e957065f71f2108bcdf5210525742fd2&Accept=application/vnd.github.everest-preview+json&event_type=custom.preview"
+        val url = "https://api.github.com/repos/${organizer}/${repository}/dispatches"
         khttp.post(url)
     }
 }
