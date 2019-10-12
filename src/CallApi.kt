33e66e3deab9acef9f2035c4add4e4eb0e21f637
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-13 01:34:58 +0900

    Call GitHub Actions CICD API

--- src/CallApi.kt
+++ src/CallApi.kt
@@ -8,8 +8,8 @@ data class Repository(val value: String = "daizu-ChatOps")
 class CallApi(repository: Repository){
     private val TOKEN:String = System.getenv("TOKEN")
     private val command = "curl -X POST -H \"Authorization: token ${TOKEN}\" -H \"Accept: application/vnd.github.everest-preview+json\"  -d '{\"event_type\": \"custom.preview\"}' -i  https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches"
-    fun CallTest():String{
-        return command
+    fun CallTest(){
+        val proc = ProcessBuilder(command).start()
 //        command.runCommand()?.apply {
 //            return this
 //        }
