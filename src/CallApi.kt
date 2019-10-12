897bfce733bb98a55978dda65b79fc8659ba941e
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-12 23:44:20 +0900

    GitHubAcdtionCI/CD Connect

--- src/CallApi.kt
+++ src/CallApi.kt
@@ -1,14 +1,29 @@
 package jp.making.felix
 
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
 data class Repository(val value: String = "daizu-ChatOps")
 
 class CallApi(repository: Repository){
-    private val TOKEN= System.getenv("TOKEN")
+    private val TOKEN:String = System.getenv("TOKEN")
     private val command = "curl -X POST -H \"Authorization: token ${TOKEN}\" -H \"Accept: application/vnd.github.everest-preview+json\" -d '{\"event_type\": \"custom.preview\"}' -i  https://api.github.com/repos/SoyBeansLab/${repository}/dispatches"
-    private val comlist = command.split(Regex(" "))
     fun CallTest(){
-        val pb = ProcessBuilder(comlist)
-        val some = pb.start()
+        command.runCommand()
     }
+    fun String.runCommand(): String? {
+        try {
+            val parts = this.split("\\s".toRegex())
+            val proc = ProcessBuilder(*parts.toTypedArray())
+                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.PIPE)
+                .start()
 
+            proc.waitFor(60, TimeUnit.MINUTES)
+            return proc.inputStream.bufferedReader().readText()
+        } catch(e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+    }
 }
