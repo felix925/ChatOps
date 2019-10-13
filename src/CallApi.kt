9155a948729297c6e5e7c90594cfced85950ee30
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-14 01:40:59 +0900

    Call GitHub Actions CICD API

--- src/CallApi.kt
+++ src/CallApi.kt
@@ -8,7 +8,7 @@ data class Repository(val value: String = "daizu-ChatOps")
 
 class CallApi(repository: Repository){
     private val TOKEN:String = System.getenv("TOKEN")
-    private val command = "curl¥GET¥-H¥\"Authorization:token ${TOKEN}\"¥-H¥\"Accept:application/vnd.github.everest-preview+json\"¥-d¥'{\"event_type\":\"custom.preview\"}'¥-i¥https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches"
+    private val command = "curl¥POST¥-H¥\"Authorization:token ${TOKEN}\"¥-H¥\"Accept:application/vnd.github.everest-preview+json\"¥-d¥'{\"event_type\":\"custom.preview\"}'¥-i¥https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches"
     //private val command = "curl -i https://api.github.com/users/defunkt"
     fun CallTest():String{
         val commands = command.replace("\\","")
@@ -21,8 +21,8 @@ class CallApi(repository: Repository){
         try {
             val parts = this.split("¥".toRegex())
             val proc = ProcessBuilder(*parts.toTypedArray())
-                .redirectOutput(ProcessBuilder.Redirect.PIPE)
-                .redirectError(ProcessBuilder.Redirect.PIPE)
+//                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+//                .redirectError(ProcessBuilder.Redirect.PIPE)
                 .start()
 
             proc.waitFor(60, TimeUnit.MINUTES)
