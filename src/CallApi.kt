532e41304c511ce995b684db4309818956afba57
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-14 00:27:28 +0900

    Call GitHub Actions CICD API

--- src/CallApi.kt
+++ src/CallApi.kt
@@ -9,14 +9,14 @@ class CallApi(repository: Repository){
     private val TOKEN:String = System.getenv("TOKEN")
     private val command = "curl¥POST¥-H¥'Authorization:token ${TOKEN}'¥-H¥\"Accept:application/vnd.github.everest-preview+json\"¥-d¥'{\"event_type\":\"custom.preview\"}'¥ -i ¥https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches"
     //private val command = "curl -i https://api.github.com/users/defunkt"
-    fun CallTest():List<String>{
-        command.runCommand()?.apply {
+    fun CallTest():String{
+        val commands = command.replace("\\","")
+        commands.runCommand()?.apply {
             return this
         }
-        var list = "fail".split("".toRegex())
-        return list
+        return ""
     }
-    fun String.runCommand(): List<String> {
+    fun String.runCommand():String? {
         try {
             val parts = this.split("¥".toRegex())
             val proc = ProcessBuilder(*parts.toTypedArray())
@@ -25,12 +25,10 @@ class CallApi(repository: Repository){
                 .start()
 
             proc.waitFor(60, TimeUnit.MINUTES)
-            //return proc.inputStream.bufferedReader().readText()
-            return parts
+            return proc.inputStream.bufferedReader().readText()
         } catch(e: IOException) {
             e.printStackTrace()
-            var list = "fail".split("".toRegex())
-            return list
+            return null
         }
     }
 }
