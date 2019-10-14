04916067ba106fb68da0a07d9d3ab1fcbb800515
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-14 22:24:46 +0900

    Call GitHub Actions CICD API

--- src/CallApi.kt
+++ src/CallApi.kt
@@ -11,12 +11,14 @@ class CallApi(repository: Repository){
     private val TOKEN:String = System.getenv("APITOKEN")
     private val APPID:String = System.getenv("CL_ID")
     private val APPSEC:String = System.getenv("CL_SEC")
-    private val getkey:String = "curl https://github.com/login/oauth/authorize?client_id=${APPID}&scope=workflow"
+    private val CODE:String = System.getenv("CODE")
+    private val token:String = "curl -X POST -d \"code=$CODE\" -d \"client_id=$APPID\" -d \"client_secret=$APPSEC\" https://github.com/login/oauth/access_token"
     //private val command = "curl¥ REST ¥-H ¥\"Accept: application/vnd.github.everest-preview+json\" ¥-d ¥'{\"event_type\":\"custom.preview\"}' ¥-i ¥https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches?access_token=${TOKEN}"
     private val command = ""
     fun CallTest():String{
         val commands = command.replace("\\","")
-        getkey.runCommand()?.apply {
+        val tokens = token.replace("\\","")
+        tokens.runCommand()?.trimCode()?.apply {
             return this
         }
         return ""
