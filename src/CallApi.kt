152a0a1a1dea590c7b03c9da8f10bfe2fad9dd30
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-14 10:58:15 +0900

    Call GitHub Actions CICD API

--- src/CallApi.kt
+++ src/CallApi.kt
@@ -7,15 +7,21 @@ import java.util.concurrent.TimeUnit
 data class Repository(val value: String = "daizu-ChatOps")
 
 class CallApi(repository: Repository){
-    private val TOKEN:String = System.getenv("TOKEN")
-    //private val command = "curl¥ POST ¥-H ¥\"Accept: application/vnd.github.everest-preview+json\" ¥-d ¥'{\"event_type\":\"custom.preview\"}' ¥-i ¥https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches?access_token=${TOKEN}"
-    private val command = "curl -u felix925 -d '{\"scopes”:[“workflow”],”note”:”ChatOpsAPI”}’  https://api.github.com/authorizations"
+    //private val TOKEN:String = System.getenv("TOKEN")
+    private val TOKEN:String = System.getenv("APITOKEN")
+    private val APPID:String = System.getenv("CL_ID")
+    private val APPSEC:String = System.getenv("CL_SEC")
+    private val code:String = System.getenv("CODE")
+    private val getkey:String = ""
+    private val command = "curl¥ POST ¥-H ¥\"Accept: application/vnd.github.everest-preview+json\" ¥-d ¥'{\"event_type\":\"custom.preview\"}' ¥-i ¥https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches?access_token=${TOKEN}"
+    //private val command = "curl -u felix925 -d '{\"scopes”:[“workflow”],”note”:”ChatOpsAPI”}’  https://api.github.com/authorizations"
     fun CallTest():String{
         val commands = command.replace("\\","")
-        commands.runCommand()?.apply {
+        commands.runCommand()?.apply{
             return this
         }
         return ""
+
     }
     fun String.runCommand():String? {
         try {
