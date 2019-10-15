0dd638f72c5d0c3b263c50783498c236b71d7bbe
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-15 10:27:57 +0900

    oauth for github

--- src/CallApi.kt
+++ src/CallApi.kt
@@ -11,7 +11,7 @@ class CallApi(repository: Repository){
     private val TOKEN:String = System.getenv("APITOKEN")
     private val APPID:String = System.getenv("CL_ID")
     private val APPSEC:String = System.getenv("CL_SEC")
-    private var code:String = "open https://github.com/login/oauth/authorize?client_id=$APPID&scope=repo"
+    private var code:String = "start https://github.com/login/oauth/authorize?client_id=$APPID&scope=repo"
     private val token:String = "curl -X POST -d \"code=$code\" -d \"client_id=$APPID\" -d \"client_secret=$APPSEC\" https://github.com/login/oauth/access_token"
     private val command = "curl¥ REST ¥-H ¥\"Accept: application/vnd.github.everest-preview+json\" ¥-d ¥'{\"event_type\":\"custom.preview\"}' ¥-i ¥https://api.github.com/repos/SoyBeansLab/${repository.value}/dispatches?access_token=${TOKEN}"
     fun CallTest():String{
