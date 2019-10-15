deac6a2bd812f21c4ffdde92393acbaaef0fbf1e
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-15 09:17:16 +0900

    oauth for github

--- src/Application.kt
+++ src/Application.kt
@@ -26,12 +26,12 @@ fun Application.module() {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
-        get("/test") {
-            val repo = Repository()
-            val caller = CallApi(repo)
-            call.respond(caller.accessCode())
-        }
-        post("/test?code={code}") {
+//        get("/test") {
+//            val repo = Repository()
+//            val caller = CallApi(repo)
+//            call.respond(caller.accessCode())
+//        }
+        get("/test?code={code}") {
             val code: String? = call.parameters["code"]
             code?.apply {
                 call.respond(this + "success")
