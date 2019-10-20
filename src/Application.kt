85c42cfea7a0e92909f9cc9ef3fca222c16a20fd
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-20 16:56:40 +0900

    feat:Call Test

--- src/Application.kt
+++ src/Application.kt
@@ -3,16 +3,11 @@ package jp.making.felix
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.auth.*
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.jackson.jackson
 import io.ktor.locations.Location
 import io.ktor.locations.location
-import io.ktor.locations.locations
-import io.ktor.locations.url
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.*
@@ -24,58 +19,25 @@ fun Application.module() {
     install(ContentNegotiation) {
         jackson {}
     }
-    @Location("/login/{type?}") class login(val type: String = "github")
-
-//    val TOKEN: String = System.getenv("APITOKEN")
+    install(Routing)
+    val TOKEN: String = System.getenv("APITOKEN")
     val APPID: String = System.getenv("CL_ID")
     val APPSEC: String = System.getenv("CL_SEC")
-//    var code: String = "curl https://github.com/login/oauth/authorize?client_id=$APPID&scope=repo,workflow"
-//    val tokens: String = "curl -X POST -d \"code=\" -d \"client_id=$APPID\" -d \"client_secret=$APPSEC\" https://github.com/login/oauth/access_token"
-//    val commands: String = "curl¥-X POST -H \"Authorization:token ${TOKEN}\" -H \"Accept: application/vnd.github.everest-preview+json\" -d '{\"event_type\": \"custom.preview\"}' -i https://api.github.com/repos/SoyBeansLab/daizu-ChatOps/dispatches"
+    @Location("/test") class some
 
-    val loginProviders = listOf(
-        OAuthServerSettings.OAuth2ServerSettings(
-            name = "github",
-            authorizeUrl = "https://github.com/login/oauth/authorize",
-            accessTokenUrl = "https://github.com/login/oauth/access_token",
-            clientId = APPID,
-            clientSecret = APPSEC
-        )
-    ).associateBy {it.name}
 
-    install(Authentication) {
-        oauth("gitHubOAuth") {
-            client = HttpClient(Apache)
-            providerLookup = { loginProviders[application.locations.resolve<login>(login::class, this).type] }
-            urlProvider = { url(login(it.name)) }
-        }
-    }
+
+
 
 
     routing {
-
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
-        authenticate("gitHubOAuth") {
-            location<login>() {
-                param("error") {
-                    handle {
-                        call.respond("error")
-                    }
-                }
-
-                handle {
-                    val principal = call.authentication.principal<OAuthAccessTokenResponse>()
-                    if (principal != null) {
-                        call.respond(principal)
-                    } else {
-                        call.respond("failed")
-                    }
-                }
+        location<some>{
+            get("") {
+                call.respond("somenow")
             }
         }
     }
 }
-
-
