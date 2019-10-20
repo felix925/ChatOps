654504ff74f08d2889d6a64aa0eb8c4cf2f0cf8f
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-20 14:23:37 +0900

    feat:Call Test

--- src/Application.kt
+++ src/Application.kt
@@ -22,8 +22,8 @@ fun Application.module() {
     val TOKEN: String = System.getenv("APITOKEN")
     val APPID: String = System.getenv("CL_ID")
     val APPSEC: String = System.getenv("CL_SEC")
-//    var code: String = "curl https://github.com/login/oauth/authorize?client_id=$APPID&scope=repo,workflow"
-//    val tokens: String = "curl -X POST -d \"code=\" -d \"client_id=$APPID\" -d \"client_secret=$APPSEC\" https://github.com/login/oauth/access_token"
+    var code: String = "curl https://github.com/login/oauth/authorize?client_id=$APPID&scope=repo,workflow"
+    val tokens: String = "curl -X POST -d \"code=\" -d \"client_id=$APPID\" -d \"client_secret=$APPSEC\" https://github.com/login/oauth/access_token"
     val commands: String = "url -X POST -H \"Authorization: token ${TOKEN}\" -H \"Accept: application/vnd.github.everest-preview+json\" -d '{\"event_type\": \"custom.preview\"}' -i  https://api.github.com/repos/SoyBeansLab/daizu-ChatOps/dispatches"
 
     routing {
@@ -33,7 +33,7 @@ fun Application.module() {
         }
         post("/test") {
             val caller = CallApi()
-            val callResult:String = caller.CallTest(commands)
+            val callResult:String = caller.CallTest(code)
             call.respond(callResult)
         }
     }
