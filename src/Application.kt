b0f948871c8efe597ba0293af88aed7166a02e8b
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-20 20:44:38 +0900

    feat:Call Test

--- src/Application.kt
+++ src/Application.kt
@@ -7,10 +7,12 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.jackson.jackson
 import io.ktor.locations.Location
-import io.ktor.locations.location
+import io.ktor.locations.Locations
+import io.ktor.locations.get
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.*
+import io.ktor.routing.get
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -20,24 +22,21 @@ fun Application.module() {
         jackson {}
     }
     install(Routing)
+    install(Locations)
     val TOKEN: String = System.getenv("APITOKEN")
     val APPID: String = System.getenv("CL_ID")
     val APPSEC: String = System.getenv("CL_SEC")
-    //@Location("/test") class some
 
+    @Location("/test")
+        data class Message(val message: String = "")
 
-
-
-
-
-    routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        routing {
+            get("/") {
+                call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            }
+            get<Message>{param->
+                call.respond("somenow")
+            }
         }
-//        location<some>{
-//            get("") {
-//                call.respond("somenow")
-//            }
-//        }
     }
 }
