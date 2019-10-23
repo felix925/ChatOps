6c3009a9acd3149ccad491200c7a6d4215f6510f
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-10-23 17:00:33 +0900

    feat:Get Token Github

--- src/Application.kt
+++ src/Application.kt
@@ -45,7 +45,7 @@ fun Application.module() {
             accessTokenUrl = "https://github.com/login/oauth/access_token",
             clientId = APPID,
             clientSecret = APPSEC,
-            defaultScopes = listOf("workflow")
+            defaultScopes = listOf("workflow","repo:status")
     )
     install(ContentNegotiation) {
         jackson {}
